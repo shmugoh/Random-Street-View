d8b24b24a4f84bf3d5cccadf7e6d6d106f8b1915
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2021-10-12 21:27:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+This script is used to test the module
+and verify if it's working correctly, so
+don't mind.
+'''
+
+import panorama
+# import tiles
+
+panorama.download_panorama("aP36qct8nn-X5V6j6DeFyw")
